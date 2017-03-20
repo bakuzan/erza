3b691491aafe440ae030d299ea30b5456b7980aa
bakuzan <imannoyed4days@hotmail.co.uk>
2017-03-20 13:12:29 +0000

    Update anime-create.js
    Revised "plan of attack" for forms.
    
    note 
    This seems okay at the moment. 
    I don't know why I would want the data to be in redux state while in edit ( <= Famous last words?! )

--- src/containers/anime-create/anime-create.js
+++ src/containers/anime-create/anime-create.js
@@ -1,30 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { createAnime } from '../../actions/index'
 import { Strings } from '../../constants/strings'
 
-let AnimeCreate = ({ dispatch }) => (
-  <div>
-    <form name="animeForm" noValidate="" onSubmit={ event => {
-      event.preventDefault();
-      console.log(event);
-      dispatch(createAnime());
-    }}>
-      <div className="has-float-label text-input-container">
-        <input type="text"
-               name="title"
-               placeholder="title"
-               autoFocus
-         />
-        <label>title</label>
-      </div>
+class AnimeCreate extends Component {
+  
+  constructor() {
+    super();
+    this.state = {}; // TODO initalise state 
+  }
+  
+  handleUserInput(event) {
+    const { name, type, value, checked } = event.target;
+    const updatedValue = type === 'checkbox' ? checked : value; // TODO move string to constants
+    this.setState({ [name]: updatedValue });
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(event, this.state);
+    // Get the state here and dispatch to a thunk
+    // thunk will save item to db, then dispatch action
+    // action will update redux state
+    this.props.dispatch(createAnime());
+  }
+  
+  render() {
+    <div>
+      <form name="animeForm" noValidate="" onSubmit={e => this.handleSubmit(e)}>
+        <div className="has-float-label text-input-container">
+          <input type="text"
+                 name="title"
+                 placeholder="title"
+                 autoFocus
+                 onChange={(e) => this.handleUserInput(e)}
+           />
+          <label>title</label>
+        </div>
 
-      <button type="submit">
-      { Strings.create }
-      </button>
-    </form>
-  </div>
-)
+        <button type="submit">
+        { Strings.create }
+        </button>
+      </form>
+    </div>
+  }
+  
+}
 
 AnimeCreate = connect()(AnimeCreate)
 
