7935a43ab6d989f50e99c29f708c63af499ba893
bakuzan <imannoyed4days@hotmail.co.uk>
2017-03-21 09:22:11 +0000

    Update anime-list.js
    Added prop types

--- src/components/anime-list/anime-list.js
+++ src/components/anime-list/anime-list.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 
 const AnimeList = ({ items }) => (
   <ul className="anime-list">
     {
       items.map(item =>
-        <li key={item.id}>
+        <li key={item.id} className="anime-item">
           { item.title }
         </li>
       )
     }
   </ul>
 )
+
+AnimeList.PropTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired
+}
 
 export default AnimeList
