51137aebb3cc4b6109ba06da9d53a0ae7f09ee51
bakuzan <imannoyed4days@hotmail.co.uk>
2017-03-21 13:38:12 +0000

    Added mal anime strings.

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,8 +1,28 @@
 export const Strings = {
   checkbox: 'checkbox',
+  // models
+  anime: {
+    status: {
+      ongoing: 1, 
+      completed: 2,
+      onhold: 3, 
+      dropped: 4, 
+      planned: 6
+    },
+    type: {
+      unknown: 0, 
+      tv: 1, 
+      ova: 2, 
+      movie: 3, 
+      special: 4, 
+      ona: 5, 
+      music: 6
+    }
+  },
   // page text
   create: 'Create',
   edit: 'Edit',
+  cancel: 'cancel',
   filters: {
     all: 'all',
     ongoing: 'ongoing',
