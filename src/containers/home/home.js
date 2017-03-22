58acbddb1efcef8fac245df7832f9d2765d09e61
bakuzan <imannoyed4days@hotmail.co.uk>
2017-03-22 09:11:30 +0000

    Update home.js

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
-import {Strings} from '../../constants/strings';
+import {Strings} from '../../constants/values';
 import {Paths} from '../../constants/paths';
 import './home.css';
 
