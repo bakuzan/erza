7da31439899c14cc0ea6a60d675443bfbe4a64bd
bakuzan <imannoyed4days@hotmail.co.uk>
2017-03-22 09:22:17 +0000

    Added update anime reducer

--- src/reducers/anime.js
+++ src/reducers/anime.js
@@ -1,18 +1,25 @@
 import update from 'immutability-helper';
-import { ADD_ANIME } from '../constants/actions'
+import { ADD_ANIME, UPDATE_ANIME } from '../constants/actions'
 import { createReducer } from './utils'
 
-const addAnime = (state, action) => {
+const updateById = (state, action) => {
   return update(state, {
     byId: {
       [action.item.id]: { $set: action.item }
-    },
+    }
+  })
+}
+
+const addAnime = (state, action) => {
+  const updatedById = updateById(state, action);
+  return update(updatedById, {
     allIds: { $push: [action.item.id] }
   })
 }
 
 const anime = createReducer({ byId: {}, allIds: [] }, {
-    [ADD_ANIME] : addAnime
+    [ADD_ANIME] : addAnime,
+    [UPDATE_ANIME] : updateById
 });
 
 export default anime
