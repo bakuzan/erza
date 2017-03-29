b94d00904d421db338d1d52fddc02d35b2b11763
bakuzan <imannoyed4days@hotmail.co.uk>
2017-03-29 11:57:03 +0100

    Update common.js

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,1 +1,8 @@
+import {Strings} from '../constants/values'
+
 export const capitalise = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+export const parseIfInt = (val) => parseInt(val) || val;
+export const getEventValue = ({ type, checked, value }) => 
+  type === Strings.checkbox   ? checked : 
+  type === Strings.selectbox  ? parseIfInt(value) : 
+                                value;
