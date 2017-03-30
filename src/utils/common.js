e341d93824810f812651a9aa1a847b286116e925
bakuzan <imannoyed4days@hotmail.co.uk>
2017-03-30 09:22:48 +0100

    tuning for get event value

--- src/utils/common.js
+++ src/utils/common.js
@@ -3,6 +3,6 @@ import {Strings} from '../constants/values'
 export const capitalise = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 export const parseIfInt = (val) => parseInt(val, 10) || val;
 export const getEventValue = ({ type, checked, value }) =>
-  type === Strings.checkbox ? checked :
-  type === Strings.date     ? value   :
-                              parseIfInt(value);
+  type === Strings.checkbox                      ? checked :
+  type === Strings.date || type === Strings.text ? value   :
+                                                   parseIfInt(value);
