52bcac6afe7ff802bd4dc30f6f5175feb8518e42
bakuzan <imannoyed4days@hotmail.co.uk>
2017-04-13 12:02:23 +0100

    Changed how access theme strings

--- src/reducers/theme.js
+++ src/reducers/theme.js
@@ -1,7 +1,7 @@
 import {SET_THEME_CLASS} from '../constants/actions'
 import {Strings} from '../constants/values'
 
-export const theme = (state = Strings.themeOne, action) => {
+export const theme = (state = Strings.themes[0], action) => {
   switch(action.type) {
     case SET_THEME_CLASS: return action.theme;
     default: return state;
