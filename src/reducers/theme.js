7a80a8ea3236805c3095df075033cbde1a4b997d
bakuzan <imannoyed4days@hotmail.co.uk>
2017-04-13 12:22:37 +0100

    Saves choice to local storage
    Used to boot up from.

--- src/reducers/theme.js
+++ src/reducers/theme.js
@@ -1,9 +1,25 @@
 import {SET_THEME_CLASS} from '../constants/actions'
 import {Strings} from '../constants/values'
 
-export const theme = (state = Strings.themes[0], action) => {
+const getUserSettings = () => JSON.parse(localStorage.getItem(Strings.localUserSettings)) || null;
+
+const getUserTheme = () => {
+  const settings = getUserSettings();
+  if (!settings || !settings.theme) return Strings.themes[0];
+  return settings.theme;
+}
+
+const persistUserThemeChoice = (state, action) => {
+  const settings = getUserSettings();
+  const updated = Object.assign({}, settings, { theme: action.theme });
+  localStorage.setItem(Strings.localUserSettings, JSON.stringify(updated));
+  return action.theme;
+}
+
+
+export const theme = (state = getUserTheme(), action) => {
   switch(action.type) {
-    case SET_THEME_CLASS: return action.theme;
+    case SET_THEME_CLASS: return persistUserThemeChoice(state, action);
     default: return state;
   }
 }
