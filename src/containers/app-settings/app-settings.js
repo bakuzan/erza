b6abb1457fd30cc467bcafd378cb71936d4c4bed
bakuzan <imannoyed4days@hotmail.co.uk>
2017-04-18 09:34:03 +0100

    Working on timed theme

--- src/containers/app-settings/app-settings.js
+++ src/containers/app-settings/app-settings.js
@@ -3,12 +3,32 @@ import {connect} from 'react-redux'
 import Tickbox from '../../components/tickbox/tickbox';
 import {setApplicationTheme, toggleTimedTheme} from '../../actions/theme'
 import {Strings} from '../../constants/values'
+import {Paths} from '../../constants/paths'
 
+let timedTheme;
 const applyThemeToBody = theme => document.body.className = theme;
+const setTimedThemeCheck = (theme) => {
+  fetch(Paths.sunrise_sunset)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json.results)
+      const { sunrise, sunset } = json.results;
+      clearTimeout(timedTheme);
+      timedTheme = setTimeout(() => {
+        const start = new Date(sunrise).getTime();
+        const end = new Date(sunset).getTime();
+        const now = Date.now();
+
+      }, 1000 * 60 * 5);
+    })
+    .catch(error => console.error(error));
+}
 
 const AppSettings = ({ theme, isTimed, setApplicationTheme, toggleTimedTheme }) => {
   const themes = Strings.themes.slice(0);
   applyThemeToBody(theme);
+  if (isTimed) setTimedThemeCheck(theme);
+  
   return (
     <div id="app-settings">
       <button type="button"
