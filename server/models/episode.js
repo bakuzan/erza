2c0ed2d8814ea67871251fd4bf6235ff792395ad
bakuzan <imannoyed4days@hotmail.co.uk>
2017-04-19 13:26:59 +0100

    Create episode.js

---
+++ server/models/episode.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const EpisodeSchema = new Schema({
+  parent: {
+    type: ObjectId,
+    ref: 'Anime',
+    required: 'Parent Id is required'
+  },
+  date: {
+    type: Date
+  },
+  episode: {
+    type: Number,
+    required: 'Episode is a required field'
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  note: {
+    type: String,
+    default: '',
+    trim: true
+  }
+});
+
+module.exports = mongoose.model('Episode', EpisodeSchema);
