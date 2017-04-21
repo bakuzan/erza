8b1b613faa36f8a5bd70f19c4f39d163fa57be73
bakuzan <imannoyed4days@hotmail.co.uk>
2017-04-21 16:13:32 +0100

    Curried paging thunk!
    Needs testing!

--- src/actions/list-settings.js
+++ src/actions/list-settings.js
@@ -1,4 +1,10 @@
 import { TOGGLE_SORT, SET_SORT_KEY, NEXT_PAGE, PREV_PAGE, SET_ITEMS_PER_PAGE, LOAD_PAGE_INFO } from '../constants/actions'
+import { loadAnime } from './anime'
+import { Strings } from '../constants/values'
+
+const FetchData = {
+  [Strings.anime] : loadAnime
+}
 
 export const toggleSortOrder = (event) => ({
   type: TOGGLE_SORT,
@@ -18,19 +24,14 @@ const fetchPrevPage = () => ({
   type: PREV_PAGE
 })
 
-export const nextPage = (type) => {
-  return function () {
-    // TODO based on type select loadData for that type
-    fetchNextPage();
-  }
+const changePage = (direction, changePage) => (type, filters) => {
+    return function(dispatch) {
+      dispatch(fetchData[type](filters, direction));
+      dispatch(changePage());
+    }
 }
-
-export const prevPage = (type) => {
-  return function () {
-    // TODO based on type select loadData for that type
-    fetchPrevPage();
-  }
-}
+export const nextPage = (type, filters) => changePage(Strings.next, fetchNextPage);
+export const prevPage = (type, filters) => changePage(Strings.prev, fetchPrevPage);
 
 export const setItemsPerPage = (event) => ({
   type: SET_ITEMS_PER_PAGE,
