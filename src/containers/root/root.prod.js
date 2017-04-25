2395fe17add98ac081d950a1ba2b7c94f2455331
bakuzan <imannoyed4days@hotmail.co.uk>
2017-04-25 15:02:37 +0100

    Root production build

---
+++ src/containers/root/root.prod.js
@@ -0,0 +1,18 @@
+import React, { PropTypes } from 'react'
+import { Provider } from 'react-redux'
+import Routes from '../../routes'
+
+const Root = ({ store, history }) => (
+  <Provider store={store}>
+    <div>
+      <Routes history={history} />
+    </div>
+  </Provider>
+)
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+}
+
+export default Root
