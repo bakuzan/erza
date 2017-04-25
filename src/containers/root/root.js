29e469c7dbb1ddac692f4f8c2424639eb62fc5ad
bakuzan <imannoyed4days@hotmail.co.uk>
2017-04-25 15:02:55 +0100

    Use to choose build

--- src/containers/root/root.js
+++ src/containers/root/root.js
@@ -1,23 +1,5 @@
-import React, { PropTypes } from 'react'
-import { Provider } from 'react-redux'
-import Routes from '../../routes'
-import DevTools from '../dev-tools'
-
-const Root = ({ store, history }) => (
-  <Provider store={store}>
-    <div>
-      <Routes history={history} />
-      {
-        !(process.env.NODE_ENV === 'production') &&
-        <DevTools />
-      }
-    </div>
-  </Provider>
-)
-
-Root.propTypes = {
-  store: PropTypes.object.isRequired,
-  history: PropTypes.object.isRequired
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./root.prod');
+} else {
+  module.exports = require('./root.dev');
 }
-
-export default Root
