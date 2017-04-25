91fb92407e92eeb2ec225ce9eb2709e4e331d993
bakuzan <imannoyed4days@hotmail.co.uk>
2017-04-25 14:58:47 +0100

    Added new tools

--- src/containers/dev-tools.js
+++ src/containers/dev-tools.js
@@ -2,10 +2,19 @@ import React from 'react'
 import { createDevTools } from 'redux-devtools'
 import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
+import DiffMonitor from 'redux-devtools-diff-monitor'
+import Dispatcher from 'redux-devtools-dispatch'
 
 export default createDevTools(
-  <DockMonitor toggleVisibilityKey="alt-h"
-               changePositionKey="alt-m">
-    <LogMonitor />
+  <DockMonitor
+    toggleVisibilityKey="alt-h"
+    changePositionKey="alt-m"
+    defaultIsVisible={false}
+  >
+    <MultipleMonitors>
+      <LogMonitor />
+      <DiffMonitor />
+      <Dispatcher />
+    </MultipleMonitors>
   </DockMonitor>
 )
