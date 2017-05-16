ea6add251133ad9133725cea1de43c0e69b9cfa2
bakuzan <imannoyed4days@hotmail.co.uk>
2017-05-16 15:44:06 +0100

    added episode actions

--- src/reducers/loading.js
+++ src/reducers/loading.js
@@ -1,20 +1,21 @@
-import { ANIME_REQUEST, ANIME_SUCCESS, TAGS_REQUEST, TAGS_SUCCESS } from '../constants/actions'
+import { 
+  ANIME_REQUEST, ANIME_SUCCESS,
+  TAGS_REQUEST, TAGS_SUCCESS,
+  EPISODE_REQUEST, EPISODE_SUCCESS
+} from '../constants/actions'
 
-const resolveRequest = (state, action) => {
-  return false;
-}
-
-const launchRequest = (state, action) => {
-  return true;
-}
+const resolveRequest = (state, action) => false;
+const launchRequest = (state, action) => true;
 
 export const isFetching = (state = false, action) => {
   switch(action.type) {
     case ANIME_REQUEST:
     case TAGS_REQUEST:
+    case EPISODE_REQUEST:
       return launchRequest(state, action);
     case ANIME_SUCCESS:
     case TAGS_SUCCESS:
+    case EPISODE_SUCCESS:
       return resolveRequest(state, action);
     default:
       return state;
