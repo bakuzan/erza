3a75929c167eb7ace70827021931f917b8a1e6f1
bakuzan <imannoyed4days@hotmail.co.uk>
2017-05-26 11:53:45 +0100

    Create shortcuts.js
    Should work, imports and navigation require checking.

---
+++ src/components/shortcuts/shortcuts.js
@@ -0,0 +1,73 @@
+import React, {Component, PropTypes} from 'react'
+import { browserHistory } from 'react-router'
+import Dialog from '../dialog/dialog'
+import AutocompleteInput from '../autocomplete-input/autocomplete-input'
+
+import {Enums} from '../../constants/values'
+import Menu from '../../constants/menu'
+
+const shortcut = o => ({ctrlKey,keyCode}) => (ctrlKey && keyCode === Enums.keyCode.q) ? o.toggleVisible() : null;
+const createListeners = f => ({
+  listen: () => document.body.addEventListener('keydown', f),
+  remove: () => document.body.removeEventListener('keydown', f)
+})
+
+class Shortcuts extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      filter: ''
+    }
+    this.items = Menu.reduce((p, c) => p.concat(c.children) , Array(0))
+    this.controller = createListeners(shortcut(this));
+    
+    this.assignRef = this.assignRef.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+    this.navigateTo = this.navigateTo.bind(this);
+  }
+  
+  componentDidMount() {
+    this.controller.listen();
+  }
+  
+  componentWillUnmount() {
+    this.controller.remove();
+  }
+  
+  toggleVisible() {
+    this.dialog.open ? this.dialog.close() : this.dialog.showModal();
+    this.setState({ filter: '' });
+  }
+  
+  assignRef(el) {
+    this.dialog = el;
+  }
+  
+  handleFilter(event) {
+    this.setState({ filter: event.target.value });
+  }
+  
+  navigateTo(id) {
+    const { link } = this.items.find(x => x.id === id);
+    this.toggleVisible();
+    browserHistory.push(link);
+  }
+  
+  render() {
+    console.log('SHORTCUTS >> ', this.state);
+    return (
+      <Dialog name="shortcuts" getDialogRef={this.assignRef}>
+        <AutocompleteInput
+          attr="title"
+          items={this.items}
+          filter={this.state.filter}
+          onChange={this.handleFilter}
+          onSelect={this.navigateTo}
+          />
+      </Dialog>
+    );
+  }
+}
+
+export default Shortcuts
