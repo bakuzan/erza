d9ecf40423b65e1856444cccac25525e6f1ac6f3
bakuzan <imannoyed4days@hotmail.co.uk>
2017-06-06 15:41:34 +0100

    Added calls to generic mutation action creator

--- src/actions/manga.js
+++ src/actions/manga.js
@@ -1,26 +1,29 @@
-// import update from 'immutability-helper'
 import { MANGA_LOAD } from '../constants/actions'
-// import { browserHistory } from 'react-router'
-// import toaster from '../utils/toaster'
-// import updatePrePost from '../utils/validators/anime-post'
-// import {mapEpisodeData} from '../utils/data'
-// import {Paths} from '../constants/paths'
-// import fetchFromServer from '../graphql/fetch'
 import MangaQL from '../graphql/query/manga'
-// import MangaML from '../graphql/mutation/manga'
-// import {constructPagingAndSorting, constructRecordForPost} from '../graphql/common'
-// import {createEpisode} from './episode'
-// import {resetPageToZero, loadPageInfo} from './paging'
+import MangaML from '../graphql/mutation/manga'
+import {
+  loadItems, 
+  loadItemsById,
+  mutateItem
+} from './list-items'
 import {Strings} from '../constants/values'
-import {loadItems, loadItemsById} from './list-items'
-
-
-// const redirectPostAction = () => browserHistory.push(`${Paths.base}${Paths.anime.list}${Strings.filters.ongoing}`);
 
 export const loadMangaData = (data) => ({
   type: MANGA_LOAD,
   data
 })
+
+export const createManga = (item) => mutateItem(
+  Strings.manga,
+  item,
+  MangaML.createManga
+)
+
+export const editManga = (item) => mutateItem(
+  Strings.manga,
+  item,
+  MangaML.updateMangaById
+)
 
 export const loadManga = (filters = {}, pageChange = null) => loadItems({
     pageChange,
