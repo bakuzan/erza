ab217549f64815adea686c6e7164c315056462d0
bakuzan <imannoyed4days@hotmail.co.uk>
2017-06-06 16:08:27 +0100

    'First take' on a generic item view.

---
+++ src/views/base-view.js
@@ -0,0 +1,176 @@
+import React, {Component, PropTypes} from 'react'
+import {browserHistory, Link} from 'react-router'
+import { connect } from 'react-redux'
+import RatingControl from '../../components/rating-control/rating-control'
+import LoadingSpinner from '../../components/loading-spinner/loading-spinner'
+import HistoryList from '../../components/history-list/history-list'
+import {getKeyByValue} from '../../utils/common'
+import {formatDateForDisplay} from '../../utils/date'
+import {getUniquePropertiesForItemType} from '../../utils/data'
+import {Paths} from '../../constants/paths'
+import {Strings, Enums, Icons} from '../../constants/values'
+
+const loadData = props => props.loadItemById(props.params.id);
+const loadHistory = props => props.loadHistoryForSeries(props.params.id);
+
+class BaseView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasHistory: false
+    }
+  }
+
+  componentDidMount() {
+    loadData(this.props);
+  }
+
+  fetchHistory() {
+    loadHistory(this.props);
+    this.setState({ hasHistory: true });
+  }
+
+  render() {
+    const { type, item, isFetching } = this.props;
+    const {current, total} = getUniquePropertiesForItemType(type);
+    if (isFetching) return (<LoadingSpinner size="fullscreen" />);
+
+    return (
+      <section>
+        <div className="flex-row reverse">
+          <div className="flex-all padding-10">
+            <header className="flex-row center-contents">
+              <h2 className="no-margin">{ item.title }</h2>
+              <div className="flex-spacer"></div>
+              <div className="button-group">
+                <button type="button"
+                        onClick={browserHistory.goBack}
+                        className="button ripple">
+                  { Strings.back }
+                </button>
+                <Link to={`${Paths.base}${Paths.anime.edit}${item._id}`}
+                      className="button ripple">
+                  { Strings.edit }
+                </Link>
+              </div>
+            </header>
+            <div className="view-content">
+
+              <span>{ `${item[current]} / ${item[total] || '??'}` }</span>
+              {
+                type === Strings.manga &&
+                <span>{ `${item.volume} / ${item.series_volumes || '??'}` }</span>
+              }
+              <RatingControl name="rating"
+                value={item.rating || 0}
+                />
+              <ul className="list column two">
+                <li className="label">{Strings.start}</li>
+                <li className="value">{formatDateForDisplay(item.start)}</li>
+
+                <li className="label">{Strings.end}</li>
+                <li className="value">{formatDateForDisplay(item.end) || Strings.unfinished}</li>
+
+                <li className="label">{Strings.isAdult}</li>
+                <li className="value" icon={item.isAdult ? Icons.tick : Icons.cross}></li>
+
+                <li className="label">{Strings.isRepeat}</li>
+                <li className="value" icon={item.isRepeat ? Icons.tick : Icons.cross}></li>
+
+                <li className="label">{Strings.owned}</li>
+                <li className="value" icon={item.owned ? Icons.tick : Icons.cross}></li>
+
+                <li className="label">{Strings.status}</li>
+                <li className="value">{getKeyByValue(Enums.anime.status, item.status)}</li>
+                {
+                  item.status === Enums.status.completed &&
+                  <div className="formatting-container">
+                    <li className="label">{Strings.timesCompleted}</li>
+                    <li className="value">{item.timesCompleted}</li>
+                  </div>
+                }
+                {
+                  type === Strings.anime && item.season && item.season.inSeason &&
+                  <div className="formatting-container">
+                    <li className="label">{Strings.season}</li>
+                    <li className="value">{`${item.season.season} ${item.season.year}`}</li>
+                  </div>
+                }
+              </ul>
+              <div>
+              {
+                !this.state.hasHistory &&
+                <button
+                  type="button"
+                  className="button primary ripple"
+                  onClick={() => this.fetchHistory()}
+                >
+                  View history
+                </button>
+              }
+              {
+                this.state.hasHistory &&
+                <div>
+                {
+                  !this.props.history.length &&
+                  <p>No history found.</p>
+                }
+                {
+                  !!this.props.history.length &&
+                  <HistoryList
+                    items={this.props.history}
+                  />
+                }
+                </div>
+              }
+              </div>
+            </div>
+          </div>
+          <div className="series-image-container full">
+            <img src={item.image} alt={`Cover for ${item.title}`} />
+            <h4>Series tags</h4>
+            <ul className="list column one">
+            {
+              !item.tagList &&
+              <li>
+                <p>{Strings.noItemsAvailable}</p>
+              </li>
+            }
+            {
+              !!item.tagList &&
+              item.tagList.map(item => (
+                <li key={item._id}>
+                  { item.name }
+                </li>
+              ))
+            }
+            </ul>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+BaseView.propTypes = {
+  type: PropTypes.string.isRequired,
+  item: PropTypes.object.isRequired,
+  history: PropTypes.arrayOf(PropTypes.object),
+  isFetching: PropTypes.bool.isRequired,
+  loadItemById: PropTypes.func.isRequired,
+  loadHistoryForSeries: PropTypes.func.isRequired
+}
+
+const selectItemFromId = (anime, id) => anime.byId[id] || {};
+const selectHistoryByParent = episode => episode.allIds.map(_id => episode.byId[_id]);
+
+const mapStateToProps = (state, ownProps) => ({
+  item: selectItemFromId(state.entities.anime, ownProps.params.id),
+  history: selectHistoryByParent(state.entities.episode),
+  isFetching: state.isFetching
+})
+
+export default connect(
+  mapStateToProps
+)(BaseView)
