9d9c50551f90b3ed294cde5def84d2f59e6ad539
bakuzan <imannoyed4days@hotmail.co.uk>
2017-06-08 12:38:33 +0100

    Create chapter-model.js

--- src/models/chapter-model.js
+++ src/models/chapter-model.js
@@ -2,7 +2,7 @@ import BaseHistoryModel from './base-history-model.js'
 
 class ChapterModel extends BaseHistoryModel {
   constructor(props = {}) {
-    super();
+    super(props);
     Object.assign(this, this.initaliseDefaults(), props);
   }
 
