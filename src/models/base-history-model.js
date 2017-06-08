04a081fa5cecb3ffa9baad79b762aaeee0a5902f
bakuzan <imannoyed4days@hotmail.co.uk>
2017-06-08 09:11:35 +0100

    Create base-history-model.js

---
+++ src/models/base-history-model.js
@@ -0,0 +1,16 @@
+class BaseHistoryModel {
+  constructor(props = {}) {
+    Object.assign(this, this.initaliseDefaults(), props);
+  }
+
+  initaliseDefaults() {
+    return {
+      parent: null,
+      date: Date.now(),
+      note: '',
+      rating: null
+    }
+  }
+}
+
+export default BaseHistoryModel
