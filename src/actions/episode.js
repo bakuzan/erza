63caae8c835c07e264ac684198602e1a85965d8a
bakuzan <imannoyed4days@hotmail.co.uk>
2017-06-14 09:30:43 +0100

    Moved load data to list-items

--- src/actions/episode.js
+++ src/actions/episode.js
@@ -1,13 +1,8 @@
-import {EPISODE_LOAD} from '../constants/actions'
 import EpisodeQL from '../graphql/query/episode'
 import EpisodeML from '../graphql/mutation/episode'
 import {loadHistoryForSeries, mutateHistoryItem, loadHistoryByDateRange} from './list-items'
 import {Strings} from '../constants/values'
 
-export const loadEpisodeData = (data) => ({
-  type: EPISODE_LOAD,
-  data
-})
 
 export const createEpisode = (item) => mutateHistoryItem(
   item,
