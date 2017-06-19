876d11819b4097be547841887352a76448188bea
bakuzan <imannoyed4days@hotmail.co.uk>
2017-06-19 11:22:55 +0100

    Create manga-create.js

---
+++ src/views/manga/manga-create.js
@@ -0,0 +1,37 @@
+import React, {PropTypes} from 'react'
+import {connect} from 'react-redux'
+
+import BaseCreate from '../base-create'
+import { loadMangaById, createManga, editManga } from '../../actions/manga'
+import {Strings} from '../../constants/values'
+
+const MangaCreate = ({ itemId, loadMangaById, createManga, editManga }) => (
+  <BaseCreate
+    type={Strings.manga}
+    itemId={itemId}
+    actions={{
+      loadById: loadMangaById,
+      create: createManga,
+      edit: editManga
+    }}
+  />
+)
+
+MangaCreate.propTypes = {
+  itemId: PropTypes.string
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  itemId: ownProps.params.id
+})
+
+const mapDispatchToProps = {
+  loadMangaById,
+  createManga,
+  editManga
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MangaCreate)
