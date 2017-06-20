8db3b0704dc44ff0bf0fee67e30e711eef0418c0
bakuzan <imannoyed4days@hotmail.co.uk>
2017-06-20 09:20:26 +0100

    First version. Doesn't handle volumes.

--- src/utils/validators/base-creation.js
+++ src/utils/validators/base-creation.js
@@ -1,23 +1,23 @@
-import updatePrePost from './anime-post'
 import {Enums, Properties} from '../../constants/values'
 import {formatDateForInput, dateStringToISOString} from '../date'
+import {getUniquePropertiesForItemType, getHistoryNameForItemType} from '../data'
 
 const historyChangeHandler = ({ current, total }) => item => {
   const changes = {};
-  if(item.episode === item.series_episodes && item.series_episodes !== 0 && !item.isRepeat) {
+  if(item[current] === item[total] && item[total] !== 0 && !item.isRepeat) {
     changes.end = formatDateForInput(new Date());
     changes.status = Enums.status.completed;
   } else if (
     item.status === Enums.status.completed &&
-    item.episode !== item.series_episodes &&
-    item.series_episodes !== 0 && !item.isRepeat
+    item[current] !== item[total] &&
+    item[total] !== 0 && !item.isRepeat
    ) {
      changes.end = null;
      changes.status = Enums.status.ongoing;
   }
 
-  if(item.episode > item.series_episodes && item.series_episodes !== 0) {
-    changes.episode = item.series_episodes;
+  if(item[current] > item[total] && item[total] !== 0) {
+    changes[current] = item[total];
   }
   return Object.assign({}, changes);
 }
@@ -33,16 +33,19 @@ const statusChangeHandler = item => {
 
 const repeatChangeHandler = ({ current, total }) => item => { [current]: item.isRepeat ? 0 : item[total] };
 
-const processValidatorChanges = history => (item, property) => {
+const processValidatorChanges = ({ history, uniqueProperties }) => (item, property) => {
   switch(property) {
-    case Properties[history]  : return historyChangeHandler(item);
+    case Properties[history]  : return historyChangeHandler(uniqueProperties)(item);
     case Properties.status    : return statusChangeHandler(item);
-    case Properties.isRepeat  : return repeatChangeHandler(item);
+    case Properties.isRepeat  : return repeatChangeHandler(uniqueProperties)(item);
     default                   : return {};
   }
 }
 
-const validateChanges = props => (model, updateProperty) => Object.assign({}, model, processValidatorChanges(props)(model, updateProperty));
+const validateChanges = props => (model, updateProperty) => {
+  const processor = processValidatorChanges(props);
+  return Object.assign({}, model, processor(model, updateProperty));
+}
 
 const validateSubmission = updateFunction => model => {
   const { start, end, series_start, series_end } = model;
@@ -57,14 +60,13 @@ const validateSubmission = updateFunction => model => {
   );
 }
 
-const validator = {
-
-}
-
 const baseValidator = (type, updateFunction) => {
   const uniqueProperties = getUniquePropertiesForItemType(type);
+  const history = getHistoryNameForItemType(type);
+  
   return {
-    validateChanges: validateChanges(),
+    validateChanges: validateChanges({ uniqueProperties, history }),
+    validateSubmission: validationSubmission(updateFunction)
   }
 }
 
