731acfdc7564484f89032d6b97910975802db430
bakuzan <imannoyed4days@hotmail.co.uk>
2017-06-26 11:01:59 +0100

    Added missing list

--- src/containers/paged-lists/paged-anime-list.js
+++ src/containers/paged-lists/paged-anime-list.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
+import AnimeList from '../../components/list-components/anime-list/anime-list'
 import BasePagedList from './base-paged-list'
 import {Strings} from '../../constants/values'
 import {addEpisodes} from '../../actions/anime'
@@ -7,6 +8,7 @@ import {addEpisodes} from '../../actions/anime'
 const PagedAnimeList = ({ items, filters, addEpisodes }) => (
   <BasePagedList
     type={String.anime}
+    list={AnimeList}
     items={items}
     filter={filters}
     addHistoryToItem={addEpisodes}
