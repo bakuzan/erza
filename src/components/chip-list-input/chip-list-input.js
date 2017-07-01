86aa23b590880e04ac9da410ebb70ba0bd562070
bakuzan <imannoyed4days@hotmail.co.uk>
2017-07-01 22:00:47 +0100

    added refactored chip list. need to make use of it and remove old.

---
+++ src/components/chip-list-input/chip-list-input.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import AutocompleteInput from '../autocomplete-input/autocomplete-input'
+import {Enums, Icons} from '../../constants/values'
+import './chip-list-input.css'
+
+class ChipListInput extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      [props.attr]: '',
+      readyRemoval: false
+    };
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.selectAutocompleteSuggestion = this.selectAutocompleteSuggestion.bind(this);
+  }
+
+  selectAutocompleteSuggestion(id) {
+    console.log('selected id > ', id);
+    const item = this.props.chipOptions.find(x => x._id === id);
+    this.updateList(item);
+    this.setState({ [this.props.attr]: '' });
+  }
+
+  persistListState(list) {
+    this.props.updateChipList(this.props.name, list);
+    this.setStateRemoval(false);
+  }
+
+  updateList(item) {
+    const list = [...this.props.chipsSelected, item];
+    this.persistListState(list);
+  }
+
+  removeInputItem(name) {
+    const list = this.props.chipsSelected.filter(x => x[this.props.attr] !== name);
+    this.persistListState(list);
+  }
+
+  removeLastInputItem() {
+    console.log('remove last');
+    const list = this.props.chipsSelected.slice(0, this.props.chipsSelected.length - 1);
+    this.persistListState(list);
+  }
+
+  setStateRemoval(value) {
+    console.log('set removal');
+    this.setState({ readyRemoval: value });
+  }
+
+  handleUserInput(event) {
+    const { value } = event.target;
+    this.setState({ [this.props.attr]: value.toLowerCase(), readyRemoval: false })
+  }
+
+  handleKeyDown(event) {
+    const { keyCode } = event;
+    console.log('chip key down! > ', keyCode);
+    if (keyCode === Enums.keyCode.backspace && !this.state[this.props.attr]) {
+      event.preventDefault();
+      if (!this.state.readyRemoval) return this.setStateRemoval(true);
+      if (this.state.readyRemoval) return this.removeLastInputItem();
+    }
+  }
+
+  render() {
+    const { attr, chipOptions, chipsSelected } = this.props;
+    console.log('%c input list => ', 'font-weight: bold; font-size: 18px', chipOptions);
+    const chips = chipsSelected.filter(x => x !== undefined).map((item, index, array) => {
+      const readyRemoval = this.state.readyRemoval && index === array.length - 1;
+      return (
+        <span key={index} className={`input-chip input-chip-deletable${readyRemoval ? ' active' : ''}`}>
+          <span className="input-chip-text">{ item[attr] }</span>
+          <button type="button"
+                  className="button-icon small input-chip-delete"
+                  title="remove"
+                  icon={Icons.cross}
+                  onClick={() => this.removeInputItem(item[attr])}
+            ></button>
+        </span>
+      )
+    });
+    const hasChips = chips.length > 0;
+    // const hasAutoComplete = chipOptions.length > 0;
+
+    return (
+      <div className="chip-list-input-container">
+        <AutocompleteInput
+          attr={attr}
+          items={chipOptions}
+          filter={this.state[attr]}
+          onChange={this.handleUserInput}
+          onSelect={this.selectAutocompleteSuggestion}
+          onKeyDown={this.handleKeyDown}
+        />
+        {
+          !!hasChips &&
+          <div className="list">
+              <div className="chip-list-inner">
+              { chips }
+              </div>
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+ChipListInput.propTypes = {
+  attr: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  chipsSelected: PropTypes.arrayOf(PropTypes.object).isRequired,
+  chipOptions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  updateChipList: PropTypes.func.isRequired
+}
+
+export default ChipListInput
