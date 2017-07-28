629a1c44748a0403d320a031a4a9e6c27be4ba6f
bakuzan <imannoyed4days@hotmail.co.uk>
2017-07-28 09:04:34 +0100

    Ensure value is boolean.
    Untested.
    Current theory as to why the query is returning an empty array is that the match values aren't matching anything - Obvious, right?
    The only difference between the successful, direct query and the unsuccessful, programmatic query is that the parameters are passed by url, in other words as a string.

--- server/statistics/index.js
+++ server/statistics/index.js
@@ -1,5 +1,5 @@
 const Constants = require('../constants');
-const { handleErrorResponse, getKeyByValue } = require('../utils/common');
+const { handleErrorResponse, getKeyByValue, stringToBool } = require('../utils/common');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; // mongoose mpromise is deprecated...so use native.
 
@@ -12,7 +12,7 @@ const getQueryModelForType = t => t === Constants.type.anime ? Anime : Manga;
 const getStatusCounts = (req, res) => {
 	const { params: { type, isAdult } } = req;
   const model = getQueryModelForType(type);
-	model.getGroupedCount("$status", 1, { isAdult: isAdult }, function(err, arr) {
+	model.getGroupedCount("$status", 1, { isAdult: stringToBool(isAdult) }, function(err, arr) {
 		console.log(arr);
     res.jsonp(
 			arr.map(({ _id, value }) => ({ key: getKeyByValue(Constants.status, _id), value }))
@@ -23,7 +23,7 @@ const getStatusCounts = (req, res) => {
 const getRatingCounts = (req, res) => {
 	const { params: { type, isAdult } } = req;
   const model = getQueryModelForType(type);
-	model.getGroupedCount("$rating", -1, { isAdult: isAdult }, function(err, arr) {
+	model.getGroupedCount("$rating", -1, { isAdult: stringToBool(isAdult) }, function(err, arr) {
     res.jsonp(
 			arr.map(({ _id, value }) => ({ key: `${_id}`, value }))
 		);
