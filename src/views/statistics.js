0fad71349232824ae227c297b0da2af089f09438
bakuzan <imannoyed4days@hotmail.co.uk>
2017-08-01 10:03:19 +0100

    Create statistics.js
    Skeleton ready for running elm app inside.

---
+++ src/views/statistics.js
@@ -0,0 +1,23 @@
+import React, {PropTypes} from 'react'
+import {connect} from 'react-redux'
+
+
+const Statistics = ({ isFetching, isAdult, type }) => (
+
+)
+
+Statistics.propTypes = {
+  isFetching: PropTypes.bool.isRequired,
+  isAdult: PropTypes.bool.isRequired,
+  type: PropTypes.string.isRequired,
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  isFetching: state.isFetching,
+  isAdult: state.isAdult,
+  type: ownProps.params.type
+})
+
+export default connect(
+  mapStateToProps
+)(Statistics)
