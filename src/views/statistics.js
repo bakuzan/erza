1d3f9eb50fb8085efd3097788860848e8fd3d4a4
bakuzan <imannoyed4days@hotmail.co.uk>
2017-08-24 21:59:18 +0100

    working integrated satellizer. css issues fixed. theme fixed. isAdult = true still broken.

--- src/views/statistics.js
+++ src/views/statistics.js
@@ -8,32 +8,32 @@ import '../../satellizer/build/static/css/satellizer.css'
 
 
 class Statistics extends Component {
-  
+
   constructor(props) {
     super(props);
-    
+
     this.setupPorts = this.setupPorts.bind(this);
   }
-  
+
   shouldComponentUpdate() {
     return false;
   }
-  
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.contentType !== this.props.contentType || nextProps.isAdult !== this.props.isAdult) {
-      this.ports.contentType.send(nextProps,contentType);
+      this.ports.contentType.send(nextProps.contentType);
       this.ports.isAdult.send(nextProps.isAdult);
     }
   }
-  
+
   setupPorts(ports) {
     this.ports = ports;
   }
-  
+
   render() {
     const { isAdult, contentType } = this.props;
     const flags = { isAdult, contentType };
-    
+
     return (
       <div id="satellizer">
         <Elm src={Main} flags={flags} ports={this.setupPorts} />
