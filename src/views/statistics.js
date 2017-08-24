a522cd872a4db05b83caa3c6c34989fca48f26e4
bakuzan <imannoyed4days@hotmail.co.uk>
2017-08-24 10:32:54 +0100

    Refactor to allow props to flow into ports.
    Untested.

--- src/views/statistics.js
+++ src/views/statistics.js
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react'
+import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
 import Elm from 'react-elm-components'
 
@@ -6,20 +6,42 @@ import {Main} from '../../satellizer/build/static/js/satellizer'
 import '../../satellizer/build/static/css/satellizer.css'
 
 
-function setupPorts(ports) {
-  ports.contentType.send("anime");
-  ports.isAdult.send(false);
+
+class Statistics extends Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.setupPorts = this.setupPorts.bind(this);
+  }
+  
+  shouldComponentUpdate() {
+    return false;
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.contentType !== this.props.contentType || nextProps.isAdult !== this.props.isAdult) {
+      this.ports.contentType.send(nextProps,contentType);
+      this.ports.isAdult.send(nextProps.isAdult);
+    }
+  }
+  
+  setupPorts(ports) {
+    this.ports = ports;
+  }
+  
+  render() {
+    const { isAdult, contentType } = this.props;
+    const flags = { isAdult, contentType };
+    
+    return (
+      <div id="satellizer">
+        <Elm src={Main} flags={flags} ports={this.setupPorts} />
+      </div>
+    );
+  }
 }
 
-
-const Statistics = ({ isAdult, contentType }) => {
-  const flags = { isAdult, contentType }
-  return (
-    <div id="satellizer">
-      <Elm src={Main} flags={flags} ports={setupPorts} />
-    </div>
-  )
-}
 
 Statistics.propTypes = {
   isFetching: PropTypes.bool.isRequired,
@@ -27,11 +49,13 @@ Statistics.propTypes = {
   contentType: PropTypes.string.isRequired,
 }
 
+
 const mapStateToProps = (state, ownProps) => ({
   isFetching: state.isFetching,
   isAdult: state.isAdult,
   contentType: ownProps.params.type
 })
+
 
 export default connect(
   mapStateToProps
