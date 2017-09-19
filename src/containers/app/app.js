88063a3f0069cba811464785371cc371fda3e6bb
bakuzan <imannoyed4days@hotmail.co.uk>
2017-09-19 11:15:24 +0100

    Added sidebar to App

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import classNames from 'classnames'
+
 import Header from '../../components/header/header';
+import Sidebar from '../sidebar/sidebar'
 import Toaster from '../../components/toaster/toaster';
 import Shortcuts from '../../components/shortcuts/shortcuts';
 import './app.css';
 
-const App = ({ params, children }) => (
-  <div className="erza">
-    <Header />
-    <main>
-    { children }
-    </main>
-    <Toaster />
-    <Shortcuts />
-  </div>
-)
+const App = ({ params, children, sidebarState }) => {
+  const appClasses = classNames("erza", { 
+    "sidebar-collapsed": sidebarState.isCollapsed, 
+    "sidebar-hidden": sidebarState.isHidden
+  })
+  
+  return (
+    <div className={appClasses}>
+      <Header />
+      <Sidebar />
+      <main>
+      { children }
+      </main>
+      <Toaster />
+      <Shortcuts />
+    </div>
+  )
+}
 
-export default App;
+const mapStateToProps = state => ({
+  sidebarState: state.sidebar
+})
+
+
+export default connect(
+  mapStateToProps
+)(App);
