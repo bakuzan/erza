b7932ff8264e35c6e1a0fb001ab787312d8ecb7f
bakuzan <imannoyed4days@hotmail.co.uk>
2017-09-19 12:29:14 +0100

    Added menu iterator

--- src/containers/sidebar/sidebar.js
+++ src/containers/sidebar/sidebar.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router'
 import classNames from 'classnames'
 
 import { toggleSidebarCollapse } from '../../actions/sidebar'
+import Menu from '../../constants/menu'
 import { Icons } from '../../constants/values'
 
 import "./Sidebar.css"
 
+const menuOptions = Menu.reduce((p, c) => p.concat(c.children), Array(0))
 
 const Sidebar = ({ isHidden, isCollapsed, toggleCollapse }) => {
   const sidebarClasses = classNames({ "collapsed": isCollapsed, "hidden": isHidden })
@@ -21,7 +23,22 @@ const Sidebar = ({ isHidden, isCollapsed, toggleCollapse }) => {
         icon={isCollapsed ? Icons.right : Icons.left} 
         onClick={toggleCollapse}>
       </button>
-
+      <ul id="sidebar-menu">
+      {
+          menuOptions.map(option => (
+            <li key={option.id} className="sidebar-item" title={option.title}>
+              <Link className="button primary" to={option.link}>
+                <div className="sidebar-item-icon center-contents">
+                  { option.icon }
+                </div>
+                <div className="sidebar-item-text">
+                  { option.title }
+                </div>
+              </Link>
+            </li>
+          ))
+      }
+      </ul>
     </div>
   )
 }
