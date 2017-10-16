69d60f3618c806120d48f35a4b3f8a95046fc79a
bakuzan <imannoyed4days@hotmail.co.uk>
2017-10-16 22:23:05 +0100

    updated sass usage. updated satellizer.

--- server/routes.js
+++ server/routes.js
@@ -30,7 +30,7 @@ router.get('/api/statistics/status-counts/:type/:isAdult', statistics.getStatusC
 router.get('/api/statistics/rating-counts/:type/:isAdult', statistics.getRatingCounts);
 router.get('/api/statistics/history-counts/:type/:isAdult/:breakdown', statistics.getHistoryCounts);
 router.get('/api/statistics/history-detail/:type/:isAdult/:breakdown/:partition', statistics.getHistoryCountsPartition);
-router.get('/api/statistics/history-years/:type/:isAdult/:breakdown/:partition', statistics.getHistoryCountsByYears);
+router.get('/api/statistics/history-years/:type/:isAdult/:breakdown/:partition', statistics.getHistoryCountsByYearsPartition);
 router.get('/api/statistics/history-years-detail/:type/:isAdult/:breakdown/:partition', statistics.getHistoryCountsByYearsPartition);
 
 // Graphql route
