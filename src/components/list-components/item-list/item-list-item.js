95edf66e78cd72fded57f1a00de53b1f47796e05
bakuzan <imannoyed4days@hotmail.co.uk>
2018-01-13 12:26:26 +0000

    handle _blank vunerability.

--- src/components/list-components/item-list/item-list-item.js
+++ src/components/list-components/item-list/item-list-item.js
@@ -1,99 +1,103 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {Link} from 'react-router-dom'
-import {Paths} from '../../../constants/paths'
-import {Enums, Icons} from '../../../constants/values'
-import {getKeyByValue} from '../../../utils/common'
-import {formatDateTimeForDisplay} from '../../../utils/date'
-import {getUniquePropertiesForItemType} from '../../../utils/data'
+import { Link } from 'react-router-dom';
+
+import NewTabLink from '../../../components/new-tab-link';
+import { Paths } from '../../../constants/paths';
+import { Enums, Icons } from '../../../constants/values';
+import { getKeyByValue } from '../../../utils/common';
+import { formatDateTimeForDisplay } from '../../../utils/date';
+import { getUniquePropertiesForItemType } from '../../../utils/data';
 
 const ItemListItem = ({ type, item, addAction }) => {
   const { current, total } = getUniquePropertiesForItemType(type);
   const hasMalId = !!item.malId;
   const hasLink = !!item.link;
-  const statusIcon = item.isRepeat                                ? Icons.clockwise :
-                     item.status === Enums.status.onhold    ? Icons.pause     :
-                     item.status === Enums.status.completed ? Icons.tick      :
-                                                                    null;
+  const statusIcon = item.isRepeat
+    ? Icons.clockwise
+    : item.status === Enums.status.onhold
+      ? Icons.pause
+      : item.status === Enums.status.completed ? Icons.tick : null;
 
   return (
     <li className={`${type}-item`}>
       <div>
-        <time dateTime={item.updatedDate}>{ formatDateTimeForDisplay(item.updatedDate) }</time>
-        <h4>{ item.title }</h4>
+        <time dateTime={item.updatedDate}>
+          {formatDateTimeForDisplay(item.updatedDate)}
+        </time>
+        <h4>{item.title}</h4>
         <div className="flex-row start-center-contents">
-            {
-              !!addAction &&
-              <button
-                type="button"
-                className="button-icon small rounded primary"
-                icon="+"
-                onClick={() => addAction(item._id)}
-                disabled={item.status === Enums.status.completed && !item.isRepeat}
-                ></button>
-            }
-          <span>{ `${item[current]}/${item[total] || '??'}` }</span>
-          {
-            !!statusIcon &&
-            <span className="button-icon small bold"
-                  icon={statusIcon}
-                  title={getKeyByValue(Enums.status, item.status)}
-                  ></span>
-          }
+          {!!addAction && (
+            <button
+              type="button"
+              className="button-icon small rounded primary"
+              icon="+"
+              onClick={() => addAction(item._id)}
+              disabled={
+                item.status === Enums.status.completed && !item.isRepeat
+              }
+            />
+          )}
+          <span>{`${item[current]}/${item[total] || '??'}`}</span>
+          {!!statusIcon && (
+            <span
+              className="button-icon small bold"
+              icon={statusIcon}
+              title={getKeyByValue(Enums.status, item.status)}
+            />
+          )}
         </div>
         <div className="button-group fixed-width">
-          <Link to={`${Paths.base}${Paths[type].view}${item._id}`}
-                className="button ripple">
+          <Link
+            to={`${Paths.base}${Paths[type].view}${item._id}`}
+            className="button ripple"
+          >
             View
           </Link>
-          <Link to={`${Paths.base}${Paths[type].edit}${item._id}`}
-                className="button ripple">
+          <Link
+            to={`${Paths.base}${Paths[type].edit}${item._id}`}
+            className="button ripple"
+          >
             Edit
           </Link>
         </div>
       </div>
-      <div className="flex-spacer"></div>
-      {
-        (hasMalId || hasLink) &&
+      <div className="flex-spacer" />
+      {(hasMalId || hasLink) && (
         <div className="mal-image">
-        {
-          hasMalId &&
-          <a
-           href={`https://myanimelist.net/${type}/${item.malId}`}
-           className="mal-link"
-           target="_blank"
-           title="Open MAL entry in new tab."
-          >
-            <img
-              src="https://myanimelist.net/favicon.ico"
-              alt="MyAnimelist icon"
+          {hasMalId && (
+            <NewTabLink
+              href={`https://myanimelist.net/${type}/${item.malId}`}
+              className="mal-link"
+              title="Open MAL entry in new tab."
+            >
+              <img
+                src="https://myanimelist.net/favicon.ico"
+                alt="MyAnimelist icon"
+              />
+            </NewTabLink>
+          )}
+          {hasLink && (
+            <NewTabLink
+              href={item.link}
+              className="button-icon small"
+              icon={Icons.link}
+              title="Open content link"
             />
-          </a>
-        }
-        {
-          hasLink &&
-          <a
-           href={item.link}
-           className="button-icon small"
-           icon={Icons.link}
-           target="_blank"
-           title="Open content link"
-          >
-          </a>
-        }
+          )}
         </div>
-      }
+      )}
       <div className="series-image-container">
         <img src={item.image} alt={`Cover for ${item.title}`} />
       </div>
     </li>
   );
-}
+};
 
 ItemListItem.propTypes = {
   type: PropTypes.string.isRequired,
   item: PropTypes.object.isRequired,
   addAction: PropTypes.func
-}
+};
 
-export default ItemListItem
+export default ItemListItem;
