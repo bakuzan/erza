69dfd3b3a382d02e1c7635e8e9d3ee265bc11065
bakuzan <imannoyed4days@hotmail.co.uk>
2018-02-03 19:54:43 +0000

    fix for link active value not changing.

--- src/routes.js
+++ src/routes.js
@@ -1,52 +1,81 @@
-import React from 'react'
-import {Route, Switch, Redirect} from 'react-router-dom';
-import {ConnectedRouter} from 'react-router-redux';
-import {Paths} from './constants/paths'
-import {Strings} from './constants/values'
-import App from './containers/app/app'
-import Home from './views/home'
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
+import { Paths } from './constants/paths';
+import { Strings } from './constants/values';
+import App from './containers/app/app';
+import Home from './views/home';
 
-import Anime from './views/anime/anime'
-import AnimeView from './views/anime/anime-view'
-import AnimeCreate from './views/anime/anime-create'
+import Anime from './views/anime/anime';
+import AnimeView from './views/anime/anime-view';
+import AnimeCreate from './views/anime/anime-create';
 
-import Manga from './views/manga/manga'
-import MangaView from './views/manga/manga-view'
-import MangaCreate from './views/manga/manga-create'
+import Manga from './views/manga/manga';
+import MangaView from './views/manga/manga-view';
+import MangaCreate from './views/manga/manga-create';
 
-import AnimeHistoryView from './views/history/anime-history'
-import MangaHistoryView from './views/history/manga-history'
+import AnimeHistoryView from './views/history/anime-history';
+import MangaHistoryView from './views/history/manga-history';
 
-import Statistics from './views/statistics'
-
+import Statistics from './views/statistics/statistics';
 
 const ErzaRoute = ({ component: PageComponent, ...routeProps }) => (
-  <Route
-    {...routeProps}
-    component={PageComponent}
-  />
-)
+  <Route {...routeProps} component={PageComponent} />
+);
 
 const ErzaRoutes = ({ match }) => (
   <Switch>
-    <ErzaRoute path={`${match.path}${Paths.anime.list}:filter`} component={Anime} />
-    <ErzaRoute path={`${match.path}${Paths.anime.view}:id`} component={AnimeView} />
-    <ErzaRoute path={`${match.path}${Paths.anime.create}`} component={AnimeCreate} />
-    <ErzaRoute path={`${match.path}${Paths.anime.edit}:id`} component={AnimeCreate} />
+    <ErzaRoute
+      path={`${match.path}${Paths.anime.list}:filter`}
+      component={Anime}
+    />
+    <ErzaRoute
+      path={`${match.path}${Paths.anime.view}:id`}
+      component={AnimeView}
+    />
+    <ErzaRoute
+      path={`${match.path}${Paths.anime.create}`}
+      component={AnimeCreate}
+    />
+    <ErzaRoute
+      path={`${match.path}${Paths.anime.edit}:id`}
+      component={AnimeCreate}
+    />
 
-    <ErzaRoute path={`${match.path}${Paths.manga.list}:filter`} component={Manga} />
-    <ErzaRoute path={`${match.path}${Paths.manga.view}:id`} component={MangaView} />
-    <ErzaRoute path={`${match.path}${Paths.manga.create}`} component={MangaCreate} />
-    <ErzaRoute path={`${match.path}${Paths.manga.edit}:id`} component={MangaCreate} />
+    <ErzaRoute
+      path={`${match.path}${Paths.manga.list}:filter`}
+      component={Manga}
+    />
+    <ErzaRoute
+      path={`${match.path}${Paths.manga.view}:id`}
+      component={MangaView}
+    />
+    <ErzaRoute
+      path={`${match.path}${Paths.manga.create}`}
+      component={MangaCreate}
+    />
+    <ErzaRoute
+      path={`${match.path}${Paths.manga.edit}:id`}
+      component={MangaCreate}
+    />
 
-    <ErzaRoute path={`${match.path}${Paths.history}${Strings.anime}`} component={AnimeHistoryView} />
-    <ErzaRoute path={`${match.path}${Paths.history}${Strings.manga}`} component={MangaHistoryView} />
+    <ErzaRoute
+      path={`${match.path}${Paths.history}${Strings.anime}`}
+      component={AnimeHistoryView}
+    />
+    <ErzaRoute
+      path={`${match.path}${Paths.history}${Strings.manga}`}
+      component={MangaHistoryView}
+    />
 
-    <ErzaRoute path={`${match.path}${Paths.statistics}:type`} component={Statistics} />
+    <ErzaRoute
+      path={`${match.path}${Paths.statistics}:type`}
+      component={Statistics}
+    />
 
     <ErzaRoute path="*" render={() => <div>Erza Page not found</div>} />
   </Switch>
-)
+);
 
 const Routes = ({ history }) => (
   <ConnectedRouter history={history}>
@@ -61,6 +90,6 @@ const Routes = ({ history }) => (
       </Switch>
     </App>
   </ConnectedRouter>
-)
+);
 
-export default Routes
+export default Routes;
---
+++ src/views/statistics/content-type-filters.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import FilterLink from '../../containers/filter-link/filter-link';
+import { Paths } from '../../constants/paths';
+import { Strings } from '../../constants/values';
+
+const ContentTypeFilters = () => (
+  <div className="button-group">
+    <FilterLink filter={`${Paths.base}${Paths.statistics}${Strings.anime}`}>
+      {Strings.anime}
+    </FilterLink>
+    <FilterLink filter={`${Paths.base}${Paths.statistics}${Strings.manga}`}>
+      {Strings.manga}
+    </FilterLink>
+  </div>
+);
+
+export default ContentTypeFilters;
