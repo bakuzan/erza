8bf6ae5f3db7ee763363801cab40a5a1c3350a3c
bakuzan <imannoyed4days@hotmail.co.uk>
2018-02-03 11:06:36 +0000

    refactored list filter children

--- src/containers/list-filter/list-filter.js
+++ src/containers/list-filter/list-filter.js
@@ -1,56 +1,57 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux'
-import FilterLink from '../filter-link/filter-link'
-import RadioButton from '../../components/radio-button/radio-button'
-import ClearableInput from '../../components/clearable-input/clearable-input'
-import SelectBox from '../../components/select-box/select-box'
-import {toggleSortOrder, setSortKey} from '../../actions/sorting'
-import {Strings} from '../../constants/values'
-import {Paths} from '../../constants/paths'
-import './list-filter.css'
+import { connect } from 'react-redux';
+import FilterLink from '../filter-link/filter-link';
+import RadioButton from '../../components/radio-button/radio-button';
+import ClearableInput from '../../components/clearable-input/clearable-input';
+import SelectBox from '../../components/select-box/select-box';
+import { toggleSortOrder, setSortKey } from '../../actions/sorting';
+import { Strings } from '../../constants/values';
+import { Paths } from '../../constants/paths';
+import './list-filter.css';
 
 const FILTER_BASE = type => `${Paths.base}${Paths[type].list}`;
 
 const SORT_OPTIONS = [
   { text: 'Title', value: 'title' },
   { text: 'Updated date', value: 'updatedDate' }
-]
+];
 
-const ListFilter = ({ type, search, isAdult, onChange, sortOrder, onSortOrderToggle, sortKey, onChangeSortKey, children }) => {
-  const filterBase = FILTER_BASE(type)
-  const statusLinks = Object.keys(Strings.filters)
-	  .map(status => {
-  		const statusUrl = Strings.filters[status]
-  		return (
-  		  <FilterLink key={statusUrl} filter={`${filterBase}${statusUrl}`}>
-  		  { statusUrl }
-  		  </FilterLink>
-  		)
-	  })
+const ListFilter = ({
+  type,
+  search,
+  isAdult,
+  onChange,
+  sortOrder,
+  onSortOrderToggle,
+  sortKey,
+  onChangeSortKey,
+  children
+}) => {
+  const filterBase = FILTER_BASE(type);
+  const statusLinks = Object.keys(Strings.filters).map(status => {
+    const statusUrl = Strings.filters[status];
+    return (
+      <FilterLink key={statusUrl} filter={`${filterBase}${statusUrl}`}>
+        {statusUrl}
+      </FilterLink>
+    );
+  });
 
   return (
     <div className="list-filter">
+      <ClearableInput value={search} onChange={onChange} />
 
-      <ClearableInput
-        value={search}
-        onChange={onChange}
-      />
-
-      <div className="button-group">
-      { statusLinks.slice(0,3) }
-      </div>
-	  <div className="button-group">
-      { statusLinks.slice(3) }
-      </div>
+      <div className="button-group">{statusLinks.slice(0, 3)}</div>
+      <div className="button-group">{statusLinks.slice(3)}</div>
 
       <SelectBox
         name="sortKey"
         text="sort on"
         value={sortKey}
-        onSelect={(e) => onChangeSortKey(e)}
+        onSelect={e => onChangeSortKey(e)}
         options={SORT_OPTIONS}
-        />
+      />
 
       <div className="radio-group" role="radiogroup">
         <RadioButton
@@ -58,22 +59,19 @@ const ListFilter = ({ type, search, isAdult, onChange, sortOrder, onSortOrderTog
           label={Strings.ascending}
           value={Strings.ascending}
           checked={sortOrder === Strings.ascending}
-          onSelect={(e) => onSortOrderToggle(e)}
+          onSelect={e => onSortOrderToggle(e)}
         />
         <RadioButton
           name="sortOrder"
           label={Strings.descending}
           value={Strings.descending}
           checked={sortOrder === Strings.descending}
-          onSelect={(e) => onSortOrderToggle(e)}
+          onSelect={e => onSortOrderToggle(e)}
         />
       </div>
-
-      {
-        children
-      }
+      <div className="list-filter-custom-content">{children}</div>
     </div>
-  )
+  );
 };
 
 ListFilter.propTypes = {
@@ -84,19 +82,16 @@ ListFilter.propTypes = {
   onSortOrderToggle: PropTypes.func.isRequired,
   sortKey: PropTypes.string.isRequired,
   onChangeSortKey: PropTypes.func.isRequired
-}
+};
 
 const mapStateToProps = (state, ownProps) => ({
   sortKey: state.sorting.sortKey,
   sortOrder: state.sorting.sortOrder
-})
+});
 
-const mapDispatchToProps = ({
+const mapDispatchToProps = {
   onSortOrderToggle: toggleSortOrder,
   onChangeSortKey: setSortKey
-})
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ListFilter)
+export default connect(mapStateToProps, mapDispatchToProps)(ListFilter);
--- src/views/base-list.js
+++ src/views/base-list.js
@@ -1,32 +1,30 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import LoadingSpinner from '../components/loading-spinner/loading-spinner'
-import ListFilter from '../containers/list-filter/list-filter'
-import DailyAnime from '../containers/daily-anime/daily-anime'
-import PagedAnimeList from '../containers/paged-lists/paged-anime-list'
-import PagedMangaList from '../containers/paged-lists/paged-manga-list'
-import {Strings} from '../constants/values'
-import {getEventValue, getTimeoutSeconds, debounce} from '../utils/common'
+import { connect } from 'react-redux';
+import LoadingSpinner from '../components/loading-spinner/loading-spinner';
+import ListFilter from '../containers/list-filter/list-filter';
+import DailyAnime from '../containers/daily-anime/daily-anime';
+import PagedAnimeList from '../containers/paged-lists/paged-anime-list';
+import PagedMangaList from '../containers/paged-lists/paged-manga-list';
+import { Strings } from '../constants/values';
+import { getEventValue, getTimeoutSeconds, debounce } from '../utils/common';
 
 const getStatusList = props => {
   const { value } = props.statusFilter;
   return !!value && !!value.length ? value : [value];
-}
+};
 
 const loadData = (props, state) => {
   const statusIn = getStatusList(props);
   props.loadDataForTypedList({ statusIn, ...state });
-}
+};
 
-const fetchPagedListForType = type => type === Strings.anime
-                                      ? PagedAnimeList
-                                      : type === Strings.manga
-                                        ? PagedMangaList
-                                        : null;
+const fetchPagedListForType = type =>
+  type === Strings.anime
+    ? PagedAnimeList
+    : type === Strings.manga ? PagedMangaList : null;
 
 class BaseListView extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -49,7 +47,7 @@ class BaseListView extends Component {
       nextProps.sortOrder !== this.props.sortOrder ||
       nextProps.itemsPerPage !== this.props.itemsPerPage
     ) {
-      loadData(nextProps, this.state)
+      loadData(nextProps, this.state);
     }
   }
 
@@ -67,35 +65,22 @@ class BaseListView extends Component {
     return (
       <div className="flex-row">
         <ListFilter
-            type={type}
-            search={this.state.search}
-            onChange={this.handleUserInput}
+          type={type}
+          search={this.state.search}
+          onChange={this.handleUserInput}
         >
-          <div className="flex-spacer"></div>
-		  {
-			  type === Strings.anime &&
-			  !isAdult &&
-			  <DailyAnime
-          routeKey={routeKey}
-				  onSelect={this.handleUserInput}
-			  />
-		  }
+          {type === Strings.anime &&
+            !isAdult && (
+              <DailyAnime routeKey={routeKey} onSelect={this.handleUserInput} />
+            )}
         </ListFilter>
-        {
-          this.props.isFetching &&
-          <LoadingSpinner size="fullscreen" />
-        }
-        {
-          !this.props.isFetching &&
-          <PagedTypedList
-              filters={filters}
-              items={items}
-           />
-        }
+        {this.props.isFetching && <LoadingSpinner size="fullscreen" />}
+        {!this.props.isFetching && (
+          <PagedTypedList filters={filters} items={items} />
+        )}
       </div>
     );
   }
-
 }
 
 BaseListView.propTypes = {
@@ -106,7 +91,7 @@ BaseListView.propTypes = {
   sortKey: PropTypes.string.isRequired,
   itemsPerPage: PropTypes.object.isRequired,
   statusFilter: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = (state, ownProps) => ({
   isFetching: state.isFetching,
@@ -114,8 +99,6 @@ const mapStateToProps = (state, ownProps) => ({
   sortOrder: state.sorting.sortOrder,
   sortKey: state.sorting.sortKey,
   itemsPerPage: state.paging.itemsPerPage
-})
+});
 
-export default connect(
-  mapStateToProps
-)(BaseListView)
+export default connect(mapStateToProps)(BaseListView);
