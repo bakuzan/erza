c53bb514e835f81ae176502c22cae51920884c37
bakuzan <imannoyed4days@hotmail.co.uk>
2018-04-29 21:38:52 +0100

    Remove unecessary

--- src/components/list-components/item-list/item-list.js
+++ src/components/list-components/item-list/item-list.js
@@ -2,18 +2,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 
-import Loaders from 'components/loaders';
-
 import './item-list.css';
 
 const ItemList = ({ className, items, isFetching }) => {
   const noItems = items.length === 0;
-  const noDataButFetching = isFetching && noItems;
   const noDataNotFetching = !isFetching && noItems;
   const hasData = !!items.length;
   return (
     <ul className={classNames('item-list', className)}>
-      {noDataButFetching && <Loaders.LoadingBouncer />}
       {noDataNotFetching && (
         <li>
           {' '}
