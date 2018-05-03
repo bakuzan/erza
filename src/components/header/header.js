ee9cc7590de7ef81c3699b4f8f15218364049477
bakuzan <imannoyed4days@hotmail.co.uk>
2018-05-03 18:02:20 +0100

    some progress.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Header, SVGLogo } from 'meiko';
 
 import AppSettings from 'containers/app-settings/app-settings';
-
-import { Paths } from '../../constants/paths';
+import { Paths } from 'constants/paths';
 
 class ErzaHeader extends Component {
   render() {
     return (
       <Header
         navLeft={
-          <Link className="ripple" id="logo-svg" to={Paths.base}>
+          <NavLink className="ripple" id="logo-svg" to={Paths.base}>
             <SVGLogo text="Erza" />
-          </Link>
+          </NavLink>
         }
         title="ERZA"
         navRight={<AppSettings />}
