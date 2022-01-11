c9cf2e9328713b0cf587838c250f83e825c370d1
bakuzan <imannoyed4days@hotmail.co.uk>
2022-01-11 17:58:29 +0000

    Ignore entries with date today (already watched)

--- server/db-scripts/get_recurrent_anime.sql
+++ server/db-scripts/get_recurrent_anime.sql
@@ -3,12 +3,14 @@ WITH recurrent_cte AS
 	SELECT 
 		a.id,
 		e.date,
+		date(e.date) day,
 		cast(strftime('%w', e.date) as integer) dayOfWeekNumber
 	FROM animes a
 	JOIN episodes e ON a.id = e.animeId AND a.episode = e.episode
 	WHERE a.isAdult = 0
 	  AND a.status = 'Ongoing'
 	  AND dayOfWeekNumber = :dayOfWeekNumber
+	  AND day <> date('now')
 )
 SELECT a.*
 FROM animes a
